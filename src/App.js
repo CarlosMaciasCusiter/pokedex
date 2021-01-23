0b367e3b2a61934f2520e46b4deb230aac7a99ad
Carlos Macias Cusiter <CarlosMaciasCusiter@gmail.com>
2021-01-23 14:33:22 -0800

    Added Card Stylings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react';
-import { getAllPokemon, getPokemon } from './services/pokemon'
+import { getAllPokemon, getPokemon } from './services/pokemon';
+import './App.css'
 import Card from './components/Card.jsx'
+import Navbar from './components/Navbar'
 
 function App() {
 
@@ -21,24 +23,25 @@ function App() {
     fetchData();
   }, [])
 
-  // const next = async () => {
-  //   setLoading(true);
-  //   let data = await getAllPokemon(nextUrl);
-  //   await loadPokemon(data.results);
-  //   setNextUrl(data.next);
-  //   setPrevUrl(data.previous);
-  //   setLoading(false);
-  // }
+  const next = async () => {
+    if (!nextUrl) return
+    setLoading(true);
+    let data = await getAllPokemon(nextUrl);
+    await loadingPokemon(data.results);
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+    setLoading(false);
+  }
 
-  // const prev = async() => {
-  //   if (!prevUrl) return
-  //   setLoading(true);
-  //   let data = await getAllPokemon(prevUrl);
-  //   await loadPokemon(data.results);
-  //   setNextUrl(data.next);
-  //   setPrevUrl(data.previous)
-  //   setLoading(false);
-  // }
+  const prev = async() => {
+    if (!prevUrl) return
+    setLoading(true);
+    let data = await getAllPokemon(prevUrl);
+    await loadingPokemon(data.results);
+    setNextUrl(data.next);
+    setPrevUrl(data.previous)
+    setLoading(false);
+  }
 
   const loadingPokemon = async (data) => {
     let _pokemonData = await Promise.all(data.map(async pokemon => {
@@ -52,8 +55,19 @@ function App() {
   return (
     <div>
       { loading ? <h1>Loading...</h1> : (
-        <div className="grid-contaiiner">
-          {pokemonData.map((pokemon, index) => <Card key={index} pokemon={pokemon}/>)}
+        <div>
+          <Navbar />
+          <div className='btn'>
+            <button onClick={prev}>Prev</button>
+            <button onClick={next}>Next</button>
+          </div>
+          <div className="grid-container">
+            {pokemonData.map((pokemon, index) => <Card key={index} pokemon={pokemon}/>)}
+          </div>
+          <div className='btn'>
+            <button onClick={prev}>Prev</button>
+            <button onClick={next}>Next</button>
+          </div>
         </div>
       )}
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import './style.css'
+import typeColors from "../helpers/pokemonTypes"
+import './Card.css'
 
 function Card({pokemon}) {
     return (
@@ -7,24 +8,24 @@ function Card({pokemon}) {
             <div className="Card__img">
                 <img src={pokemon.sprites.front_default} alt="pokemon"/>
             </div>
-            <div className="Card__Name">
+            <div className="Card__name">
                 {pokemon.name}
             </div>
             <div className="Card__types">
-                {pokemon.types.map(type => <div className="Card__type">{type.type.name}</div>)}
+                {pokemon.types.map((type, index) => <div className="Card__type" key={index} style={{ backgroundColor : typeColors[type.type.name] }}>{type.type.name}</div>)}
             </div>
             <div className="Card__info">
-                <div className="Card__Data Card__data--weight">
+                <div className="Card__data Card__data--weight">
                     <p className="title">Weight</p>
-                    <p>{pokemon.weight}</p>
+                    <p>{(pokemon.weight / 4.536).toFixed(1)} lbs</p>
                 </div>
-                <div className="Card__Data Card__data--heigh">
+                <div className="Card__data Card__data--height">
                     <p className="title">Height</p>
-                    <p>{pokemon.Height}</p>
+                    <p>{(pokemon.height / 3.048).toFixed(1)} ft</p>
                 </div>
-                <div className="Card__Data Card__data--ability">
+                <div className="Card__data Card__data--ability">
                     <p className="title">Ability</p>
-                    <p>{pokemon.abilities[0].ability.name}</p>
+                    {pokemon.abilities.map((ability, index) => <span>{ability.ability.name} </span>)}
                 </div>
             </div>
         </div>
