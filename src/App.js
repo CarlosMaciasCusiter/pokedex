8ed0adb724e34e80d46a07400a00e1df9183d4e0
Carlos Macias Cusiter <CarlosMaciasCusiter@gmail.com>
2021-01-23 12:36:11 -0800

    Extracted Data from PokeAPI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import React, {useState} from 'react';
-import axios from 'axios';
+import React, {useState, useEffect} from 'react';
+import { getAllPokemon, getPokemon } from './services/pokemon'
+import Card from './components/Card.jsx'
 
 function App() {
 
-  const [pokedex, setPokedex] = useState([]);
+  const [pokemonData, setPokemonData] = useState([]);
+  const [nextUrl, setNextUrl] = useState('');
+  const [prevUrl, setPrevUrl] = useState('');
+  const [loading, setLoading] = useState(true);
+  const initialURL = 'https://pokeapi.co/api/v2/pokemon'
 
-  const addPokemon = () => {
-    setPokedex([
-      ...pokedex,
-      {
-        id: GameId,
-        pokeName: pokemonName
-      }
-    ])
+  useEffect(()=> {
+    async function fetchData() {
+      let response = await getAllPokemon(initialURL);
+      setNextUrl(response.next);
+      setPrevUrl(response.previous);
+      await loadingPokemon(response.results);
+      setLoading(false);
+    }
+    fetchData();
+  }, [])
+
+  // const next = async () => {
+  //   setLoading(true);
+  //   let data = await getAllPokemon(nextUrl);
+  //   await loadPokemon(data.results);
+  //   setNextUrl(data.next);
+  //   setPrevUrl(data.previous);
+  //   setLoading(false);
+  // }
+
+  // const prev = async() => {
+  //   if (!prevUrl) return
+  //   setLoading(true);
+  //   let data = await getAllPokemon(prevUrl);
+  //   await loadPokemon(data.results);
+  //   setNextUrl(data.next);
+  //   setPrevUrl(data.previous)
+  //   setLoading(false);
+  // }
+
+  const loadingPokemon = async (data) => {
+    let _pokemonData = await Promise.all(data.map(async pokemon => {
+      let pokemonRecord = await getPokemon(pokemon)
+      return pokemonRecord
+    }))
+    setPokemonData(_pokemonData);
   }
 
+  console.log(pokemonData)
   return (
-    <div className="App">
-      <h1>Testing Git</h1>
+    <div>
+      { loading ? <h1>Loading...</h1> : (
+        <div className="grid-contaiiner">
+          {pokemonData.map((pokemon, index) => <Card key={index} pokemon={pokemon}/>)}
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/services/pokemon.js
@@ -0,0 +1,19 @@
+import fetch from "node-fetch"
+
+export async function getAllPokemon(url) {
+    return new Promise((resolve, reject) => {
+        fetch(url).then(res => res.json())
+            .then(data => {
+                resolve(data)
+            })
+    })
+}
+
+export function getPokemon({ url }) {
+    return new Promise((resolve, reject) => {
+        fetch(url).then(res => res.json())
+            .then(data => {
+                resolve(data)
+            })
+    });
+}
